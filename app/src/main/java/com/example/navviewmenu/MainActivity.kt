14f4119ab2ddce88c1a647a61c49cab55305b978
Frank <f.neumann.berlin@googlemail.com>
2023-04-04 12:08:31 +0200

    Grundgeruest ohne Fragmente

--- app/src/main/java/com/example/navviewmenu/MainActivity.kt
+++ app/src/main/java/com/example/navviewmenu/MainActivity.kt
@@ -1,16 +1,47 @@
 package com.example.navviewmenu
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
 import com.example.navviewmenu.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var toggle: ActionBarDrawerToggle
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.toolbar.title = "NavViewApp"
+        binding.toolbar.setTitleTextColor(Color.WHITE)
+        setSupportActionBar(binding.toolbar)
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+
+        toggle = ActionBarDrawerToggle(this,binding.drawer,R.string.open,R.string.close)
+        binding.drawer.addDrawerListener(toggle)
+        toggle.syncState()
+
+        binding.navView.setNavigationItemSelectedListener { item:MenuItem ->
+            when(item.itemId){
+                R.id.nav_home -> Toast.makeText(this, "Auf Home", Toast.LENGTH_SHORT).show()
+            }
+            binding.drawer.closeDrawer(GravityCompat.START)
+            true
+        }
     }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(toggle.onOptionsItemSelected(item))
+            return true
+
+        return super.onOptionsItemSelected(item)
+    }
+
 }
